3bc3e3ee5d46ed3ab7fdf98a505d815aeb9cf1c2
Matthew Curtis <matthew@matthewbryancurtis.com>
2017-10-15 10:33:22 -0700

    Write tests for current functionality

--- tests/colorsort.test.js
+++ tests/colorsort.test.js
@@ -2,11 +2,113 @@ import { expect } from 'chai';
 import tinycolor from 'tinycolor2';
 import ColorSort from '../src/index';
 
-describe('ColorSort', function () {
+describe('ColorSort constructor core', function () {
   it('creates an array of tinycolor objects', function () {
     const input = '#BADA55';
     const cs = new ColorSort(input);
     const result = cs.collection[0] instanceof tinycolor;
     expect(result).to.be.true;
+  });
+
+  it('disregards non-color text', function () {
+    const input = '#FF0000 and #00FF00 are complementary colors';
+    const cs = new ColorSort(input);
+    expect(cs.collection).to.have.lengthOf(2);
+  });
+
+  it('separates adjacent color text', function () {
+    const input = '#FF0000#00FF00#0000FF';
+    const cs = new ColorSort(input);
+    expect(cs.collection).to.have.lengthOf(3);
+  });
+});
+
+describe('ColorSort constructor input types', function () {
+  it('accepts 3-digit hex values', function () {
+    const input = '#BAD';
+    const cs = new ColorSort(input);
+    expect(cs.collection).to.have.lengthOf(1);
+  });
+
+  it('accepts 4-digit hex values', function () {
+    const input = '#BAD';
+    const cs = new ColorSort(input);
+    expect(cs.collection).to.have.lengthOf(1);
+  });
+
+  it('accepts 6-digit hex values', function () {
+    const input = '#BAD';
+    const cs = new ColorSort(input);
+    expect(cs.collection).to.have.lengthOf(1);
+  });
+
+  it('accepts 8-digit hex values', function () {
+    const input = '#BAD';
+    const cs = new ColorSort(input);
+    expect(cs.collection).to.have.lengthOf(1);
+  });
+
+  it('accepts rgb integer values', function () {
+    const input = 'rgb(255, 0, 153)';
+    const cs = new ColorSort(input);
+    expect(cs.collection).to.have.lengthOf(1);
+  });
+
+  it('accepts rgb percentage values', function () {
+    const input = 'rgb(100%, 0%, 60%)';
+    const cs = new ColorSort(input);
+    expect(cs.collection).to.have.lengthOf(1);
+  });
+
+  it('accepts rgba integer values', function () {
+    const input = 'rgb(255, 0, 153, 1)';
+    const cs = new ColorSort(input);
+    expect(cs.collection).to.have.lengthOf(1);
+  });
+
+  it('accepts rgba percentage values', function () {
+    const input = 'rgb(100%, 0%, 60%, 100%)';
+    const cs = new ColorSort(input);
+    expect(cs.collection).to.have.lengthOf(1);
+  });
+
+  it('accepts hsl values', function () {
+    const input = 'hsl(270,60%,70%)';
+    const cs = new ColorSort(input);
+    expect(cs.collection).to.have.lengthOf(1);
+  });
+
+  it('accepts hsla values', function () {
+    const input = 'hsla(240, 100%, 50%, 1)';
+    const cs = new ColorSort(input);
+    expect(cs.collection).to.have.lengthOf(1);
+  });
+});
+
+describe('ColorSort.formattedValues()', function () {
+  const cs = new ColorSort('#000000 rgb(170, 170, 170) hsl(0, 0%, 100%)');
+
+  it('formattedValues() returns an array of hex values', function () {
+    const result = cs.formattedValues('hex');
+    const expected = ["#000000", "#aaaaaa", "#ffffff"];
+    expect(result).to.have.deep.members(expected);
+  });
+
+  it('formattedValues(\'hex\') returns an array of hex values', function () {
+    const result = cs.formattedValues('hex');
+    const expected = ["#000000", "#aaaaaa", "#ffffff"];
+    expect(result).to.have.deep.members(expected);
+  });
+
+  it('formattedValues(\'rgb\') returns an array of rgb values', function () {
+    const result = cs.formattedValues('rgb');
+    const expected = ["rgb(0, 0, 0)", "rgb(170, 170, 170)", "rgb(255, 255, 255)"];
+    expect(result).to.have.deep.members(expected);
+  });
+
+  it('formattedValues(\'hsl\') returns an array of hsl values', function () {
+    const result = cs.formattedValues('hsl');
+    const expected = ["hsl(0, 0%, 0%)", "hsl(0, 0%, 67%)", "hsl(0, 0%, 100%)"];
+    expect(result).to.have.deep.members(expected);
   });
 });
